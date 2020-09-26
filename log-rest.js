7579da3579c3b8ab939fa4e2a0e781d6166bdaf8
Glen Rutherford <glenrutherford4@gmail.com>
2020-09-26 11:17:09 +1000

    Init

---
+++ log-rest.js
@@ -0,0 +1,87 @@
+// https://www.moesif.com/blog/technical/logging/How-we-built-a-Nodejs-Middleware-to-Log-HTTP-API-Requests-and-Responses/
+
+// To run, open a command line in the same folder as this file, then run: node log-rest.js
+const http = require("http");
+const port = 8888;
+
+const server = http.createServer((request, response) => {
+    const requestStart = Date.now();
+
+    let body = [];
+    let requestErrorMessage = null;
+
+    const getChunk = chunk => body.push(chunk);
+    const assembleBody = () => {
+        body = Buffer.concat(body).toString();
+    };
+    const getError = error => {
+        requestErrorMessage = error.message;
+    };
+    request.on("data", getChunk);
+    request.on("end", assembleBody);
+    request.on("error", getError);
+
+    const logClose = () => {
+        removeHandlers();
+        log(request, response, "Client aborted.", requestStart);
+    };
+    const logError = error => {
+        removeHandlers();
+        log(request, response, error.message, requestStart);
+    };
+    const logFinish = () => {
+        removeHandlers();
+        log(request, response, requestErrorMessage, requestStart);
+    };
+    response.on("close", logClose);
+    response.on("error", logError);
+    response.on("finish", logFinish);
+
+    const removeHandlers = () => {
+        request.off("data", getChunk);
+        request.off("end", assembleBody);
+        request.off("error", getError);
+        response.off("close", logClose);
+        response.off("error", logError);
+        response.off("finish", logFinish);
+    };
+
+    process(request, response);
+});
+
+const log = (request, response, errorMessage, requestStart) => {
+    const { rawHeaders, httpVersion, method, socket, url } = request;
+    const { remoteAddress, remoteFamily } = socket;
+
+    const { statusCode, statusMessage } = response;
+    const headers = response.getHeaders();
+
+    console.log(
+        JSON.stringify({
+            timestamp: Date.now(),
+            processingTime: Date.now() - requestStart,
+            rawHeaders,
+            // body,
+            errorMessage,
+            httpVersion,
+            method,
+            remoteAddress,
+            remoteFamily,
+            url,
+            response: {
+                statusCode,
+                statusMessage,
+                headers
+            }
+        }, null, 3)
+    );
+};
+
+const process = (request, response) => {
+    setTimeout(() => {
+        response.end();
+    }, 100);
+}
+
+server.listen(port);
+console.log("Listening on port: " + port);
